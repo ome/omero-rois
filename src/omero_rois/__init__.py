37f63bc438a121e33da6a2aee9c7058ee9b04aae
Jean-Marie Burel <j.burel@dundee.ac.uk>
2020-01-09 09:22:04 +0000

    review import

--- src/omero_rois/__init__.py
+++ src/omero_rois/__init__.py
@@ -17,7 +17,7 @@
 # with this program; if not, write to the Free Software Foundation, Inc.,
 # 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
-from library import (
+from .library import (
     NoMaskFound,
     InvalidBinaryImage,
     mask_from_binary_image,
