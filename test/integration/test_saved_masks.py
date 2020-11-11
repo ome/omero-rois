38cf1b54d053d704893b0b8376178c2e01b5b521
Simon Li <spli@dundee.ac.uk>
2020-11-11 12:38:44 +0000

    Add pre-commit

--- test/integration/test_saved_masks.py
+++ test/integration/test_saved_masks.py
@@ -40,26 +40,31 @@ from omero_rois import (
 
 @pytest.fixture
 def binary_image():
-    return np.array([
-        [0, 0, 0, 0],
-        [0, 1, 1, 0],
-        [0, 1, 0, 0],
-        [0, 0, 0, 0],
-    ], dtype=np.uint8)
+    return np.array(
+        [
+            [0, 0, 0, 0],
+            [0, 1, 1, 0],
+            [0, 1, 0, 0],
+            [0, 0, 0, 0],
+        ],
+        dtype=np.uint8,
+    )
 
 
 @pytest.fixture
 def label_image():
-    return np.array([
-        [0, 0, 0, 2],
-        [0, 1, 1, 0],
-        [0, 1, 2, 0],
-        [0, 0, 0, 0],
-    ], dtype=np.uint8)
+    return np.array(
+        [
+            [0, 0, 0, 2],
+            [0, 1, 1, 0],
+            [0, 1, 2, 0],
+            [0, 0, 0, 0],
+        ],
+        dtype=np.uint8,
+    )
 
 
 class TestSavedMasks(ITest):
-
     def setup_method(self):
         self.conn = BlitzGateway(client_obj=self.client)
 
@@ -81,15 +86,16 @@ class TestSavedMasks(ITest):
 
         return shapes
 
-    @pytest.mark.parametrize('args', [
-        {},
-        {'rgba': (255, 128, 64, 128), 'z': 1, 'c': 2, 't': 3, 'text': 'test'}
-    ])
+    @pytest.mark.parametrize(
+        "args",
+        [{}, {"rgba": (255, 128, 64, 128), "z": 1, "c": 2, "t": 3, "text": "test"}],
+    )
     def test_mask_from_binary_image(self, binary_image, args):
         im = self.conn.createImageFromNumpySeq(
-            iter([binary_image]), 'omero-rois-integration')
+            iter([binary_image]), "omero-rois-integration"
+        )
         # Reload
-        im = self.conn.getObject('Image', im.id)
+        im = self.conn.getObject("Image", im.id)
         px = im.getPrimaryPixels().getPlane()
 
         mask = mask_from_binary_image(px, **args)
@@ -102,14 +108,15 @@ class TestSavedMasks(ITest):
         assert unwrap(mask.getX()) == 1
         assert unwrap(mask.getY()) == 1
 
-        assert np.array_equal(np.frombuffer(mask.getBytes(), np.uint8),
-                              np.array([224], dtype=np.uint8))
+        assert np.array_equal(
+            np.frombuffer(mask.getBytes(), np.uint8), np.array([224], dtype=np.uint8)
+        )
 
         if args:
             assert unwrap(mask.getTheZ()) == 1
             assert unwrap(mask.getTheC()) == 2
             assert unwrap(mask.getTheT()) == 3
-            assert unwrap(mask.getTextValue()) == 'test'
+            assert unwrap(mask.getTextValue()) == "test"
         else:
             assert unwrap(mask.getTheZ()) is None
             assert unwrap(mask.getTheC()) is None
@@ -118,10 +125,9 @@ class TestSavedMasks(ITest):
 
     def test_mask_from_binary_full_image(self):
         binim = np.ones((4, 4), dtype=np.uint8)
-        im = self.conn.createImageFromNumpySeq(
-            iter([binim]), 'omero-rois-integration')
+        im = self.conn.createImageFromNumpySeq(iter([binim]), "omero-rois-integration")
         # Reload
-        im = self.conn.getObject('Image', im.id)
+        im = self.conn.getObject("Image", im.id)
         px = im.getPrimaryPixels().getPlane()
 
         mask = mask_from_binary_image(px)
@@ -131,18 +137,21 @@ class TestSavedMasks(ITest):
 
         assert unwrap(mask.getWidth()) == 4
         assert unwrap(mask.getHeight()) == 4
-        assert np.array_equal(np.frombuffer(mask.getBytes(), np.uint8),
-                              np.array([255, 255], dtype=np.uint8))
+        assert np.array_equal(
+            np.frombuffer(mask.getBytes(), np.uint8),
+            np.array([255, 255], dtype=np.uint8),
+        )
 
-    @pytest.mark.parametrize('args', [
-        {},
-        {'rgba': (255, 128, 64, 128), 'z': 1, 'c': 2, 't': 3, 'text': 'test'}
-    ])
+    @pytest.mark.parametrize(
+        "args",
+        [{}, {"rgba": (255, 128, 64, 128), "z": 1, "c": 2, "t": 3, "text": "test"}],
+    )
     def test_masks_from_label_image(self, label_image, args):
         im = self.conn.createImageFromNumpySeq(
-            iter([label_image]), 'omero-rois-integration')
+            iter([label_image]), "omero-rois-integration"
+        )
         # Reload
-        im = self.conn.getObject('Image', im.id)
+        im = self.conn.getObject("Image", im.id)
         px = im.getPrimaryPixels().getPlane()
 
         masks = masks_from_label_image(px, **args)
@@ -164,36 +173,36 @@ class TestSavedMasks(ITest):
             assert unwrap(mask.getX()) == expected[i][2]
             assert unwrap(mask.getY()) == expected[i][3]
 
-            assert np.array_equal(np.frombuffer(mask.getBytes(), np.uint8),
-                                  expected[i][4])
+            assert np.array_equal(
+                np.frombuffer(mask.getBytes(), np.uint8), expected[i][4]
+            )
 
             if args:
                 assert unwrap(mask.getTheZ()) == 1
                 assert unwrap(mask.getTheC()) == 2
                 assert unwrap(mask.getTheT()) == 3
-                assert unwrap(mask.getTextValue()) == 'test'
+                assert unwrap(mask.getTextValue()) == "test"
             else:
                 assert unwrap(mask.getTheZ()) is None
                 assert unwrap(mask.getTheC()) is None
                 assert unwrap(mask.getTheT()) is None
                 assert unwrap(mask.getTextValue()) is None
 
-    @pytest.mark.parametrize('args', [
-        {},
-        {'rgba': (255, 128, 64, 128), 'z': 1, 'c': 2, 't': 3, 'text': 'test'}
-    ])
+    @pytest.mark.parametrize(
+        "args",
+        [{}, {"rgba": (255, 128, 64, 128), "z": 1, "c": 2, "t": 3, "text": "test"}],
+    )
     def test_empty_mask_from_binary_image(self, args):
         empty_binary_image = np.array([[0]], dtype=np.uint8)
         raise_on_no_mask = False
         im = self.conn.createImageFromNumpySeq(
-            iter([empty_binary_image]), 'omero-rois-integration')
+            iter([empty_binary_image]), "omero-rois-integration"
+        )
         # Reload
-        im = self.conn.getObject('Image', im.id)
+        im = self.conn.getObject("Image", im.id)
         px = im.getPrimaryPixels().getPlane()
 
-        mask = mask_from_binary_image(
-            px, raise_on_no_mask=raise_on_no_mask,
-            **args)
+        mask = mask_from_binary_image(px, raise_on_no_mask=raise_on_no_mask, **args)
         mask = self.save_and_return_masks(im, [mask])[0]
 
         # The rest of this is more or less the same as the unit test
@@ -208,7 +217,7 @@ class TestSavedMasks(ITest):
             assert unwrap(mask.getTheZ()) == 1
             assert unwrap(mask.getTheC()) == 2
             assert unwrap(mask.getTheT()) == 3
-            assert unwrap(mask.getTextValue()) == 'test'
+            assert unwrap(mask.getTextValue()) == "test"
         else:
             assert unwrap(mask.getTheZ()) is None
             assert unwrap(mask.getTheC()) is None
