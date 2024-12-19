84ac13963f88deacd49785d428b3fb0fd53712d7
Jean-Marie Burel <j.burel@dundee.ac.uk>
2024-12-19 20:49:33 +0000

    Fix warning

--- docs/conf.py
+++ docs/conf.py
@@ -64,7 +64,7 @@ master_doc = "index"
 #
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
-language = None
+# language = None
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
@@ -192,7 +192,7 @@ epub_exclude_files = ["search.html"]
 # -- Options for intersphinx extension ---------------------------------------
 
 # Example configuration for intersphinx: refer to the Python standard library.
-intersphinx_mapping = {"https://docs.python.org/": None}
+intersphinx_mapping = {'<name>': ('https://docs.python.org/', None)}
 
 # -- Options for todo extension ----------------------------------------------
 
