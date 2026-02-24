91d9916db733d3cdc3983fd478ff2ed36764577c
William Moore <w.moore@dundee.ac.uk>
2026-02-24 16:21:50 +0000

    add tests for shape_to_binary_image() and masks_to_labels()

--- test/integration/test_saved_masks.py
+++ test/integration/test_saved_masks.py
@@ -27,6 +27,7 @@
 
 import omero.clients
 from omero.gateway import BlitzGateway
+from omero.model import RoiI
 from omero.testlib import ITest
 from omero.rtypes import unwrap
 import numpy as np
@@ -35,6 +36,8 @@ import pytest
 from omero_rois import (
     mask_from_binary_image,
     masks_from_label_image,
+    masks_to_labels,
+    shape_to_binary_image
 )
 
 
@@ -69,7 +72,7 @@ class TestSavedMasks(ITest):
         self.conn = BlitzGateway(client_obj=self.client)
 
     def save_and_return_masks(self, im, masks):
-        roi = omero.model.RoiI()
+        roi = RoiI()
         for mask in masks:
             roi.addShape(mask)
 
@@ -223,3 +226,93 @@ class TestSavedMasks(ITest):
             assert unwrap(mask.getTheC()) is None
             assert unwrap(mask.getTheT()) is None
             assert unwrap(mask.getTextValue()) is None
+
+    def test_shape_to_binary_image(self, label_image):
+        im = self.conn.createImageFromNumpySeq(
+            iter([label_image]), "test_shape_to_binary_image"
+        )
+        im = self.conn.getObject("Image", im.id)
+        px = im.getPrimaryPixels().getPlane()
+
+        masks = masks_from_label_image(px)
+        self.save_and_return_masks(im, masks)[0]
+
+        # load masks and convert back to labels
+        rois = self.conn.getRoiService().findByImage(im.id, None)
+        assert len(rois.rois) == 1
+        shapes = rois.rois[0].copyShapes()
+        assert len(shapes) == 2
+        assert type(shapes[0]) == omero.model.MaskI
+
+        expected_arrs = [
+            np.array(
+                [
+                    [1, 1],
+                    [1, 0],
+                ],
+                dtype=np.uint8,
+            ),
+            np.array(
+                [
+                    [0, 1],
+                    [0, 0],
+                    [1, 0],
+                ],
+                dtype=np.uint8,
+            ),
+        ]
+        for expected_arr, omero_shape in zip(expected_arrs, shapes):
+            shape_info = shape_to_binary_image(omero_shape)
+            np_bool_data = shape_info[0]
+            t, c, z, y, x, h, w = shape_info[1]
+            assert (h, w) == expected_arr.shape
+            assert (y, x) == (omero_shape.getY(), omero_shape.getX())
+            assert np.array_equal(np_bool_data * 1, expected_arr)
+
+    def test_masks_to_labels(self) -> None:
+        """Test export of a Zarr image."""
+        size_xy = 20
+        size_z = 5
+        size_c = 2
+        img_shape = (1, size_c, size_z, size_xy, size_xy)  # (T, C, Z, Y, X)
+        images = self.import_fake_file(sizeC=size_c, sizeX=size_xy, sizeY=size_xy,
+                                       sizeZ=size_z, client=self.client)
+        img_id = images[0].id.val
+
+        # Create a mask
+        from skimage.data import binary_blobs
+
+        blobs = binary_blobs(length=size_xy, volume_fraction=0.1, n_dim=2).astype(
+            "int8"
+        )
+        red = [255, 0, 0, 255]
+
+        # create ROIs in OMERO... - one per channel, with shape on each Z plane
+        new_rois = []
+        for theC in range(size_c):
+            roi = RoiI()
+            roi.setImage(images[0])
+            for theZ in range(size_z):
+                mask = mask_from_binary_image(blobs, rgba=red, z=theZ, c=theC, t=0)
+                roi.addShape(mask)
+            updateService = self.client.sf.getUpdateService()
+            new_rois.append(updateService.saveAndReturnObject(roi))
+
+        rois = self.conn.getRoiService().findByImage(img_id, None)
+        assert len(rois.rois) == size_c
+        shapes = []
+        for r in rois.rois:
+            shapes.extend(r.copyShapes())
+
+        labels, fillColors, properties = masks_to_labels(shapes, img_shape)
+        print("labels, fillColors, properties", labels, fillColors, properties)
+        assert labels.shape == img_shape
+        assert labels.dtype == np.int64
+        # The value of each label array corresponds to ROI ID
+        assert labels.max() == max(roi.id.val for roi in new_rois)
+
+        for theC in range(size_c):
+            for theZ in range(size_z):
+                mask = blobs.astype(bool)
+                expected_labels = np.where(mask, new_rois[theC].id.val, 0)
+                assert np.array_equal(labels[0, theC, theZ], expected_labels)
