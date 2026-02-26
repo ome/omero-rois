fc9ff96901ce7b92e3ba51327ba7ba64fff76959
William Moore <w.moore@dundee.ac.uk>
2026-02-26 15:20:07 +0000

    Bump version: 0.4.1 → 0.4.2.dev0

--- docs/conf.py
+++ docs/conf.py
@@ -25,7 +25,7 @@ copyright = "2019, Open Microscopy Environment"
 author = "Open Microscopy Environment"
 
 # The short X.Y version
-version = "0.4.1"
+version = "0.4.2.dev0"
 # The full version, including alpha/beta/rc tags
 release = version
 
